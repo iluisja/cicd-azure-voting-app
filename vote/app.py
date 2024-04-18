c09ba5429ad763a2f96720f143c028f69bde133a
Luis Rodríguez <0619627002@uma.es>
2024-04-18 09:45:56 +0000

    Updated app.py

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Luis")
+option_b = os.getenv('OPTION_B', "Pingu")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
